7d3ddfe3da45d64e41fa5d801acd16549f25764d
Tom Youel <tom@basereality.io>
2023-06-03 21:55:55 +0100

    feat/React THREE fiber added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import './App.css'
+import { FunctionComponent, useRef, useState, useEffect } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
+
+const THREEScene: FunctionComponent = () => {
+	const meshRef = useRef<THREE.Mesh>(null!);
+
+	useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+	return (
+		<>
+			<ambientLight />
+			<mesh ref={meshRef}>
+				<boxGeometry args={[1, 1, 1]} />
+				<meshStandardMaterial color={'white'} />
+			</mesh>
+		</>
+	)
+};
 
 function App() {
 
 	return (
-		<></>
+		<Canvas>
+			<THREEScene />
+		</Canvas>
 	)
 }
 
