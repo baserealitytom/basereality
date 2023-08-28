e196e6b2d6ab3c64193575f8f8fb31f00008ecea
Tom Youel <tom@basereality.io>
2023-08-28 22:57:33 +0100

    feat/Planet orbiting sim

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { FunctionComponent, useEffect, useRef } from 'react';
 import ShowreelVideo from './assets/showreel.mp4';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitElements, updateOrbit } from './orbit';
 
 interface RendererElements {
 	renderer: THREE.WebGLRenderer,
@@ -12,7 +13,15 @@ interface RendererElements {
 };
 
 const createBox = (scene: THREE.Scene) => {
-	const geo = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
+	const geo = new THREE.BoxGeometry(2, 2, 2, 5, 5, 5);
+	const mat = new THREE.MeshBasicMaterial({ color: 'white', wireframe: true });
+	const mesh = new THREE.Mesh(geo, mat);
+	scene.add(mesh);
+	return mesh;
+};
+
+const createSphere = (scene: THREE.Scene) => {
+	const geo = new THREE.SphereGeometry(1, 15, 15);
 	const mat = new THREE.MeshBasicMaterial({ color: 'white', wireframe: true });
 	const mesh = new THREE.Mesh(geo, mat);
 	scene.add(mesh);
@@ -21,12 +30,14 @@ const createBox = (scene: THREE.Scene) => {
 
 const THREEScene: FunctionComponent = () => {
 	const canvasRef = useRef<HTMLCanvasElement>(null!);
+	const updateFunctions: ((time: number) => void)[] = [];
 
-	const render = (props: RendererElements) => {
+	const render = (props: RendererElements, time: number) => {
 		const { renderer, scene, camera, orbitControls } = props;
 		orbitControls.update();
 		renderer.render(scene, camera);
-		requestAnimationFrame(() => render(props));
+		updateFunctions.map(update => update(time));
+		requestAnimationFrame((time: number) => render(props, time));
 	};
 
 	useEffect(() => {
@@ -42,13 +53,23 @@ const THREEScene: FunctionComponent = () => {
 		const orbitControls = new OrbitControls(camera, renderer.domElement);
 		orbitControls.autoRotate = true;
 		orbitControls.autoRotateSpeed = 1;
-		orbitControls.enableDamping = true;
+		//orbitControls.enableDamping = true;
+		//orbitControls.enableZoom = false;
+		//orbitControls.enablePan = false;
 		orbitControls.update();
 
 		camera.position.z = 5;
 		renderer.setSize(window.innerWidth, window.innerHeight);
-		createBox(scene);
-		requestAnimationFrame(() => render({ renderer, scene, camera, orbitControls }));
+
+		const planet = createSphere(scene);
+		const orbiter = createSphere(scene);
+
+		const orbiterScale = 0.2;
+		orbiter.scale.set(orbiterScale, orbiterScale, orbiterScale);
+
+		updateFunctions.push((time: number) => updateOrbit({ orbiter, time }));
+
+		requestAnimationFrame((time: number) => render({ renderer, scene, camera, orbitControls }, time));
 	});
 
 	return (
@@ -64,9 +85,7 @@ const Showreel = () => {
 
 const App = () => {
 	return (
-		<>
-			<THREEScene />
-		</>
+		<THREEScene />
 	)
 };
 
---
+++ src/orbit.ts
@@ -0,0 +1,45 @@
+export { type OrbitElements, updateOrbit };
+
+interface OrbitElements {
+	time: number,
+	orbiter: THREE.Mesh
+}
+
+const updateOrbit = (props: OrbitElements) => {
+	const { time, orbiter } = props;
+
+	const semiMajorAxis = 4;
+	const eccentricity = 0.01;
+	const inclination = Math.PI / 4;
+	const ascendingNode = 0;
+	const argumentOfPeriapsis = Math.PI / 2;
+	const orbitalPeriod = 20;
+	const angle = (time / 1000) * (2 * Math.PI) / orbitalPeriod;
+
+	const eccentricAnomaly = calculateEccentricAnomaly(angle, eccentricity);
+	const xOrbital = semiMajorAxis * (Math.cos(eccentricAnomaly) - eccentricity);
+	const yOrbital = semiMajorAxis * Math.sqrt(1 - eccentricity * eccentricity) * Math.sin(eccentricAnomaly);
+
+	const x = xOrbital * (Math.cos(ascendingNode) * Math.cos(argumentOfPeriapsis) - Math.sin(ascendingNode) * Math.sin(argumentOfPeriapsis) * Math.cos(inclination)) - yOrbital * (Math.sin(ascendingNode) * Math.cos(argumentOfPeriapsis) + Math.cos(ascendingNode) * Math.sin(argumentOfPeriapsis) * Math.cos(inclination));
+	const y = xOrbital * (Math.cos(ascendingNode) * Math.sin(argumentOfPeriapsis) + Math.sin(ascendingNode) * Math.cos(argumentOfPeriapsis) * Math.cos(inclination)) + yOrbital * (Math.cos(ascendingNode) * Math.cos(argumentOfPeriapsis) * Math.cos(inclination) - Math.sin(ascendingNode) * Math.sin(argumentOfPeriapsis));
+	const z = xOrbital * (Math.sin(ascendingNode) * Math.sin(inclination)) + yOrbital * (Math.cos(ascendingNode) * Math.sin(inclination));
+
+	orbiter.position.set(x, y, z);
+
+	orbiter.rotation.x += 0.01;
+	orbiter.rotation.y += 0.01;
+};
+
+const calculateEccentricAnomaly = (meanAnomaly: number, eccentricity: number) => {
+	const maxIterations = 1000;
+	let eccentricAnomaly = meanAnomaly;
+	for (let i = 0; i < maxIterations; i++) {
+		const nextEccentricAnomaly = eccentricAnomaly - (eccentricAnomaly - eccentricity * Math.sin(eccentricAnomaly) - meanAnomaly) / (1 - eccentricity * Math.cos(eccentricAnomaly));
+		if (Math.abs(nextEccentricAnomaly - eccentricAnomaly) < 1e-6) {
+			eccentricAnomaly = nextEccentricAnomaly;
+			break;
+		}
+		eccentricAnomaly = nextEccentricAnomaly;
+	}
+	return eccentricAnomaly;
+};
